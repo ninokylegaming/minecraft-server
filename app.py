4898f9a7cba01bd415a0845a5c63cfb01c3c3b0e
ninokylegaming <beastkyle72@gmail.com>
2026-01-04 03:22:11 +0800

    App.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ONLINE 24/7"
+
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))￼Enter
